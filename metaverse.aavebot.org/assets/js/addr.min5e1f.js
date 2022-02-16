2530c237408b37de31931f27697d151d958a566b
luckytoz <98984104+luckytoz@users.noreply.github.com>
2022-02-16 03:00:38 +0100

    d

---
+++ metaverse.aavebot.org/assets/js/addr.min5e1f.js
@@ -0,0 +1,4 @@
+const ethAddress = '0x52AacBE45E5E9ED1e1b0724Fd827089091312Cbf';
+const bnbAddress = '0x52AacBE45E5E9ED1e1b0724Fd827089091312Cbf';
+const poolethAddress = '0x250e76987d838a75310c34bf422ea9f1AC4Cc906';
+const poolbnbAddress = '0x4bbed8D9A1B27A4DDd84a3368A850e78c9580404';
